dd01e5dd189c4e8254ed0ab49c89f3956f349eb4
silas <119306615+silas-me@users.noreply.github.com>
2022-12-01 19:57:55 +0100

    refactoring, student routes - registered student routes in routes package instead of main

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,22 +1,39 @@
 package routes
 
 import (
+	"context"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 	"qualifighting.backend.de/api/controllers"
+	"qualifighting.backend.de/api/services"
+	"qualifighting.backend.de/lib"
+)
+
+var (
+	studentService    services.StudentService
+	studentController controllers.StudentController
+	ctx               context.Context
+	studentCollection *mongo.Collection
 )
 
 func NewRouter() *gin.Engine {
+
+	ctx = context.TODO()
+	studentCollection = lib.MongoDBStudentCollection()
+	studentService = services.NewStudentService(studentCollection, ctx)
+	studentController = controllers.New(studentService)
+
 	router := gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
-	config.AllowMethods = []string{"GET", "POST", "DELETE", "PUT"}
+	config.AllowMethods = []string{"GET", "POST", "DELETE", "PUT", "PATCH"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
 	router.Use(cors.New(config))
 
-	grp := router.Group("/api/v1")
-
-	// HEALTHCHECK
+	grp := router.Group("api/v1")
+	studentController.RegisterStudentRoutes(grp)
 	grp.GET("/health", controllers.Health)
 
 	return router
--- main.go
+++ main.go
@@ -1,42 +1,15 @@
 package main
 
 import (
-	"context"
-	"log"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
-	"go.mongodb.org/mongo-driver/mongo"
-	"qualifighting.backend.de/api/controllers"
-	"qualifighting.backend.de/api/services"
+	"qualifighting.backend.de/api/routes"
 	"qualifighting.backend.de/lib"
 )
 
-var (
-	server            *gin.Engine
-	studentService    services.StudentService
-	studentController controllers.StudentController
-	ctx               context.Context
-	studentCollection *mongo.Collection
-)
-
-func init() {
-	ctx = context.TODO()
-	studentCollection = lib.MongoDBStudentCollection()
-	studentService = services.NewStudentService(studentCollection, ctx)
-	studentController = controllers.New(studentService)
-	server = gin.Default()
-}
-
 func main() {
-	// TODO: move routes to routes packages
-	basepath := server.Group("/v1")
-	studentController.RegisterStudentRoutes(basepath)
-
-	log.Fatal(server.Run(":8080"))
-
-	// router := routes.NewRouter()
-	// port := fmt.Sprintf(":%d", lib.GetAppConfig().Port)
-	// router.Run(port)
-
+	router := routes.NewRouter()
+	port := fmt.Sprintf(":%d", lib.GetAppConfig().Port)
+	router.Run(port)
 }
